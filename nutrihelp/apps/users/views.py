67633ba5eff3c3bcfa619e3221ad7b68ce263b2e
Erick Vicente Agrazal Lopez <erick.agrazal@balboadevelopers.com>
2019-08-02 03:04:41 -0500

    Added fix to signup view.

--- nutrihelp/apps/users/views.py
+++ nutrihelp/apps/users/views.py
@@ -6,7 +6,7 @@ from django.views import generic
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('users:login')
     template_name = 'signup.html'
 
     def get_context_data(self, **kwargs):
